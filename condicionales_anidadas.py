37ea58f2e42f17da4c79c32a556bf76e90c4f819
alejandg1 <71184889+alejandg1@users.noreply.github.com>
2022-11-28 12:29:05 -0500

    Add files via upload

---
+++ condicionales_anidadas.py
@@ -0,0 +1,33 @@
+def run():
+    estudiantes= [
+        {'nombre': 'Alejandro', 'sexo': 'M', 'carrera': 'sof', 'estado S.E.': 'Medio', 'promedio': 80, 'asistencia': 100, 'beca': False, 'deuda': 0},
+        {'nombre': 'ni idea', 'sexo': 'F', 'carrera': 'bio', 'estado S.E.': 'Medio alto', 'promedio': 94, 'asistencia': 97, 'beca': False, 'deuda': 0},
+        {'nombre': 'alguien', 'sexo': 'M', 'carrera': 'soft', 'estado S.E.': 'Medio', 'promedio': 97, 'asistencia': 40, 'beca': False, 'deuda': 0},
+        {'nombre': 'Carlos', 'sexo': 'F', 'carrera': 'med', 'estado S.E.': 'medio bajo', 'promedio': 75, 'asistencia': 80, 'beca': False, 'deuda': 0},
+        {'nombre': 'Udyr', 'sexo': 'M', 'carrera': 'sof', 'estado S.E.': 'Medio', 'promedio': 78, 'asistencia': 90, 'beca': False, 'deuda': 0},
+        {'nombre': 'Martha', 'sexo': 'F', 'carrera': 'sof', 'estado S.E.': 'Medio', 'promedio': 62, 'asistencia': 55, 'beca': False, 'deuda': 0},
+        {'nombre': 'Camille', 'sexo': 'F', 'carrera': 'sof', 'estado S.E.': 'Medio', 'promedio': 30, 'asistencia': 90, 'beca': False, 'deuda': 0},
+        {'nombre': 'Andres', 'sexo': 'M', 'carrera': 'med', 'estado S.E.': 'bajo', 'promedio': 70, 'asistencia': 60, 'beca': False, 'deuda': 0},
+        {'nombre': 'Alison', 'sexo': 'f', 'carrera': 'bio', 'estado S.E.': 'medio', 'promedio': 98, 'asistencia': 80, 'beca': False, 'deuda': 0},
+        {'nombre': 'Adrian', 'sexo': 'M', 'carrera': 'med', 'estado S.E.': 'alto', 'promedio': 96, 'asistencia': 90, 'beca': False, 'deuda': 0},
+    ]
+    for i in estudiantes:
+        if  95 <= i['promedio']<= 100:
+            cumple= True
+        else:
+            cumple= False
+        if  90<=i['asistencia'] <=100:
+            cumplea= True
+        else:
+            cumplea= False
+        if i['deuda']==0:
+            cumpled= True
+        if cumple & cumplea & cumpled:
+            i['beca']= True
+    for i in estudiantes:
+        if i['beca']==True:
+            print(f"beca academica a {i['nombre']} {i['beca']} ")
+
+
+if __name__ == "__main__":
+    run()
