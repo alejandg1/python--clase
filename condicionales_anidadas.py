058e810617efe4d45db1795c37b734d6d66b3ccd
alejandg1 <71184889+alejandg1@users.noreply.github.com>
2022-11-28 12:48:14 -0500

    Add files via upload

--- condicionales_anidadas.py
+++ condicionales_anidadas.py
@@ -20,21 +20,23 @@ def beca_socioeconomica():
     pass
 
 
-def run():
-    estudiantes= [
-        {'nombre': 'Alejandro', 'sexo': 'M', 'carrera': 'sof', 'estado S.E.': 'Medio', 'promedio': 80, 'asistencia': 100, 'beca': False, 'deuda': 0},
-        {'nombre': 'ED', 'sexo': 'F', 'carrera': 'bio', 'estado S.E.': 'Medio alto', 'promedio': 99, 'asistencia': 97, 'beca': False, 'deuda': 0},
-        {'nombre': 'alguien', 'sexo': 'M', 'carrera': 'soft', 'estado S.E.': 'Medio', 'promedio': 97, 'asistencia': 40, 'beca': False, 'deuda': 0},
-        {'nombre': 'Carlos', 'sexo': 'F', 'carrera': 'med', 'estado S.E.': 'medio bajo', 'promedio': 75, 'asistencia': 80, 'beca': False, 'deuda': 0},
-        {'nombre': 'Udyr', 'sexo': 'M', 'carrera': 'sof', 'estado S.E.': 'Medio', 'promedio': 78, 'asistencia': 90, 'beca': False, 'deuda': 0},
-        {'nombre': 'Martha', 'sexo': 'F', 'carrera': 'sof', 'estado S.E.': 'Medio', 'promedio': 62, 'asistencia': 55, 'beca': False, 'deuda': 0},
-        {'nombre': 'Camille', 'sexo': 'F', 'carrera': 'sof', 'estado S.E.': 'Medio', 'promedio': 30, 'asistencia': 90, 'beca': False, 'deuda': 0},
-        {'nombre': 'Andres', 'sexo': 'M', 'carrera': 'med', 'estado S.E.': 'bajo', 'promedio': 70, 'asistencia': 60, 'beca': False, 'deuda': 0},
-        {'nombre': 'Alison', 'sexo': 'f', 'carrera': 'bio', 'estado S.E.': 'medio', 'promedio': 98, 'asistencia': 80, 'beca': False, 'deuda': 0},
-        {'nombre': 'Adrian', 'sexo': 'M', 'carrera': 'med', 'estado S.E.': 'alto', 'promedio': 96, 'asistencia': 90, 'beca': False, 'deuda': 0},
+estudiantes= [
+        {'nombre': 'Alejandro', 'sexo': 'M', 'carrera': 'sof', 'estado S.E.': 'Medio', 'promedio': 80, 'asistencia': 100, 'beca': False, 'deuda': 0 , 'tipo de beca':""},
+        {'nombre': 'ED', 'sexo': 'F', 'carrera': 'bio', 'estado S.E.': 'Medio alto', 'promedio': 99, 'asistencia': 97, 'beca': False, 'deuda': 0, 'tipo de beca':""},
+        {'nombre': 'alguien', 'sexo': 'M', 'carrera': 'soft', 'estado S.E.': 'Medio', 'promedio': 97, 'asistencia': 40, 'beca': False, 'deuda': 0, 'tipo de beca':""},
+        {'nombre': 'Carlos', 'sexo': 'F', 'carrera': 'med', 'estado S.E.': 'medio bajo', 'promedio': 75, 'asistencia': 80, 'beca': False, 'deuda': 0, 'tipo de beca':""},
+        {'nombre': 'Udyr', 'sexo': 'M', 'carrera': 'sof', 'estado S.E.': 'Medio', 'promedio': 78, 'asistencia': 90, 'beca': False, 'deuda': 0, 'tipo de beca':""},
+        {'nombre': 'Martha', 'sexo': 'F', 'carrera': 'sof', 'estado S.E.': 'Medio', 'promedio': 62, 'asistencia': 55, 'beca': False, 'deuda': 0, 'tipo de beca':""},
+        {'nombre': 'Camille', 'sexo': 'F', 'carrera': 'sof', 'estado S.E.': 'Medio', 'promedio': 30, 'asistencia': 90, 'beca': False, 'deuda': 0, 'tipo de beca':""},
+        {'nombre': 'Andres', 'sexo': 'M', 'carrera': 'med', 'estado S.E.': 'bajo', 'promedio': 70, 'asistencia': 60, 'beca': False, 'deuda': 0, 'tipo de beca':""},
+        {'nombre': 'Alison', 'sexo': 'f', 'carrera': 'bio', 'estado S.E.': 'medio', 'promedio': 98, 'asistencia': 80, 'beca': False, 'deuda': 0, 'tipo de beca':""},
+        {'nombre': 'Adrian', 'sexo': 'M', 'carrera': 'med', 'estado S.E.': 'alto', 'promedio': 96, 'asistencia': 90, 'beca': False, 'deuda': 0, 'tipo de beca':""},
     ]
+
+
+def run():
     beca_academica(estudiantes)
-    
+    #!hacer el 2do proceso xd
 
     
 
