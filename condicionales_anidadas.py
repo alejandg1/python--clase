9ea42b1e07f004e5865e7e50baaf9f128b905d2b
Alejandro Gómez <josealejandrogomezparedes@gmail.com>
2022-12-01 18:21:42 -0500

    ahora si xd

--- condicionales_anidadas.py
+++ condicionales_anidadas.py
@@ -1,19 +1,19 @@
 estudiantes = [
-    {'nombre': 'Alejandro', 'sexo': 'M', 'carrera': 'soft', 'estado S.E.': 'medio',
-        'promedio': 80, 'asistencia': 100, 'beca': False, 'deuda': 7, 'tipo de beca': ""},
+    {'nombre': 'Alejandro', 'sexo': 'M', 'carrera': 'soft', 'estado S.E.': 'bajo',
+        'promedio': 80, 'asistencia': 100, 'beca': False, 'deuda': 0, 'tipo de beca': ""},
     {'nombre': 'ED', 'sexo': 'F', 'carrera': 'bio', 'estado S.E.': 'medio alto',
      'promedio': 99, 'asistencia': 97, 'beca': False, 'deuda': 0, 'tipo de beca': ""},
     {'nombre': 'alguien', 'sexo': 'M', 'carrera': 'soft', 'estado S.E.': 'medio bajo',
      'promedio': 80, 'asistencia': 90, 'beca': False, 'deuda': 0, 'tipo de beca': ""},
     {'nombre': 'Carlos', 'sexo': 'F', 'carrera': 'med', 'estado S.E.': 'medio bajo',
-     'promedio': 75, 'asistencia': 80, 'beca': False, 'deuda': 0, 'tipo de beca': ""},
+     'promedio': 85, 'asistencia': 90, 'beca': False, 'deuda': 0, 'tipo de beca': ""},
     {'nombre': 'Udyr', 'sexo': 'M', 'carrera': 'soft', 'estado S.E.': 'medio',
      'promedio': 78, 'asistencia': 90, 'beca': False, 'deuda': 4, 'tipo de beca': ""},
     {'nombre': 'Martha', 'sexo': 'F', 'carrera': 'soft', 'estado S.E.': 'medio',
-     'promedio': 62, 'asistencia': 55, 'beca': False, 'deuda': 0, 'tipo de beca': ""},
+     'promedio': 100, 'asistencia': 100, 'beca': False, 'deuda': 0, 'tipo de beca': ""},
     {'nombre': 'Camille', 'sexo': 'F', 'carrera': 'soft', 'estado S.E.': 'bajo',
      'promedio': 80, 'asistencia': 90, 'beca': False, 'deuda': 0, 'tipo de beca': ""},
-    {'nombre': 'Andres', 'sexo': 'M', 'carrera': 'med', 'estado S.E.': 'bajo',
+    {'nombre': 'Andres', 'sexo': 'M', 'carrera': 'med', 'estado S.E.': 'alto',
      'promedio': 70, 'asistencia': 60, 'beca': False, 'deuda': 0, 'tipo de beca': ""},
     {'nombre': 'Alison', 'sexo': 'f', 'carrera': 'bio', 'estado S.E.': 'medio',
      'promedio': 98, 'asistencia': 80, 'beca': False, 'deuda': 0, 'tipo de beca': ""},
@@ -26,9 +26,9 @@ def beca_academica():
     print("---------------------------------------------------------becas academicas------------------------------------------------------")
     for i in estudiantes:
         if i['tipo de beca'] == "":
-            apto = True
+            sin_beca = True
         else:
-            apto = False
+            sin_beca = False
         if 95 <= i['promedio'] <= 100:
             cumplep = True
         else:
@@ -41,20 +41,21 @@ def beca_academica():
             cumpled = True
         else:
             cumpled = False
-        if (cumplep & cumplea & cumpled & apto):
+        if (cumplep & cumplea & cumpled & sin_beca):
             i['beca'] = True
             i['tipo de beca'] = "a"
             print(
                 f"beca otorgada a {i['nombre']}: {i['beca']}, tipo: {i['tipo de beca']}  ")
+        
 
 
 def beca_socioeconomica():
     print("-------------------------------------------------------becas socioeconomicas---------------------------------------------------")
     for i in estudiantes:
         if i['tipo de beca'] == "":
-            apto = True
+            sin_beca = True
         else:
-            apto = False
+            sin_beca = False
         if i['estado S.E.'] == "bajo" or i['estado S.E.'] == "medio bajo":
             socioe = True
         else:
@@ -71,7 +72,7 @@ def beca_socioeconomica():
             cumpled = True
         else:
             cumpled = False
-        if (cumplep & cumplea & cumpled & apto & socioe):
+        if (cumplep & cumplea & cumpled & sin_beca & socioe):
             i['beca'] = True
             i['tipo de beca'] = "s.e."
             print(
