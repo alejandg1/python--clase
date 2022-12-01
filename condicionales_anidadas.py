4a6f93fbf819c14ba529ea2db76f0172a68c68bb
Alejandro Gómez <josealejandrogomezparedes@gmail.com>
2022-12-01 18:29:42 -0500

    ahora si x2 xd

--- condicionales_anidadas.py
+++ condicionales_anidadas.py
@@ -1,31 +1,31 @@
 estudiantes = [
     {'nombre': 'Alejandro', 'sexo': 'M', 'carrera': 'soft', 'estado S.E.': 'bajo',
-        'promedio': 80, 'asistencia': 100, 'beca': False, 'deuda': 0, 'tipo de beca': ""},
+        'promedio': 80, 'asistencia': 100, 'beca': False, 'deuda': 0, 'tipo': ""},
     {'nombre': 'ED', 'sexo': 'F', 'carrera': 'bio', 'estado S.E.': 'medio alto',
-     'promedio': 99, 'asistencia': 97, 'beca': False, 'deuda': 0, 'tipo de beca': ""},
+     'promedio': 99, 'asistencia': 97, 'beca': False, 'deuda': 0, 'tipo': ""},
     {'nombre': 'alguien', 'sexo': 'M', 'carrera': 'soft', 'estado S.E.': 'medio bajo',
-     'promedio': 80, 'asistencia': 90, 'beca': False, 'deuda': 0, 'tipo de beca': ""},
+     'promedio': 80, 'asistencia': 90, 'beca': False, 'deuda': 0, 'tipo': ""},
     {'nombre': 'Carlos', 'sexo': 'F', 'carrera': 'med', 'estado S.E.': 'medio bajo',
-     'promedio': 85, 'asistencia': 90, 'beca': False, 'deuda': 0, 'tipo de beca': ""},
+     'promedio': 85, 'asistencia': 90, 'beca': False, 'deuda': 0, 'tipo': ""},
     {'nombre': 'Udyr', 'sexo': 'M', 'carrera': 'soft', 'estado S.E.': 'medio',
-     'promedio': 78, 'asistencia': 90, 'beca': False, 'deuda': 4, 'tipo de beca': ""},
+     'promedio': 78, 'asistencia': 90, 'beca': False, 'deuda': 4, 'tipo': ""},
     {'nombre': 'Martha', 'sexo': 'F', 'carrera': 'soft', 'estado S.E.': 'medio',
-     'promedio': 100, 'asistencia': 100, 'beca': False, 'deuda': 0, 'tipo de beca': ""},
+     'promedio': 100, 'asistencia': 100, 'beca': False, 'deuda': 0, 'tipo': ""},
     {'nombre': 'Camille', 'sexo': 'F', 'carrera': 'soft', 'estado S.E.': 'bajo',
-     'promedio': 80, 'asistencia': 90, 'beca': False, 'deuda': 0, 'tipo de beca': ""},
+     'promedio': 80, 'asistencia': 90, 'beca': False, 'deuda': 0, 'tipo': ""},
     {'nombre': 'Andres', 'sexo': 'M', 'carrera': 'med', 'estado S.E.': 'alto',
-     'promedio': 70, 'asistencia': 60, 'beca': False, 'deuda': 0, 'tipo de beca': ""},
+     'promedio': 70, 'asistencia': 60, 'beca': False, 'deuda': 0, 'tipo': ""},
     {'nombre': 'Alison', 'sexo': 'f', 'carrera': 'bio', 'estado S.E.': 'medio',
-     'promedio': 98, 'asistencia': 80, 'beca': False, 'deuda': 0, 'tipo de beca': ""},
+     'promedio': 98, 'asistencia': 80, 'beca': False, 'deuda': 0, 'tipo': ""},
     {'nombre': 'Adrian', 'sexo': 'M', 'carrera': 'med', 'estado S.E.': 'alto',
-     'promedio': 96, 'asistencia': 90, 'beca': False, 'deuda': 0, 'tipo de beca': ""},
+     'promedio': 96, 'asistencia': 90, 'beca': False, 'deuda': 0, 'tipo': ""},
 ]
 
 
 def beca_academica():
     print("---------------------------------------------------------becas academicas------------------------------------------------------")
     for i in estudiantes:
-        if i['tipo de beca'] == "":
+        if i['tipo'] == "":
             sin_beca = True
         else:
             sin_beca = False
@@ -43,16 +43,16 @@ def beca_academica():
             cumpled = False
         if (cumplep & cumplea & cumpled & sin_beca):
             i['beca'] = True
-            i['tipo de beca'] = "a"
+            i['tipo'] = "a"
             print(
-                f"beca otorgada a {i['nombre']}: {i['beca']}, tipo: {i['tipo de beca']}  ")
+                f"beca otorgada a {i['nombre']}: {i['beca']}, tipo: {i['tipo']}  ")
         
 
 
 def beca_socioeconomica():
     print("-------------------------------------------------------becas socioeconomicas---------------------------------------------------")
     for i in estudiantes:
-        if i['tipo de beca'] == "":
+        if i['tipo'] == "":
             sin_beca = True
         else:
             sin_beca = False
@@ -74,9 +74,9 @@ def beca_socioeconomica():
             cumpled = False
         if (cumplep & cumplea & cumpled & sin_beca & socioe):
             i['beca'] = True
-            i['tipo de beca'] = "s.e."
+            i['tipo'] = "s.e."
             print(
-                f"beca otorgada a {i['nombre']}: {i['beca']}, tipo: {i['tipo de beca']}  ")
+                f"beca otorgada a {i['nombre']}: {i['beca']}, tipo: {i['tipo']}  ")
 
 
 def run():
