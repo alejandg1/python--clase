d19e5d07175f97e14a22f930feb52160d49da9d8
Alejandro Gómez <josealejandrogomezparedes@gmail.com>
2022-12-02 13:39:35 -0500

    antes de subir el deber

--- condicionales_anidadas.py
+++ condicionales_anidadas.py
@@ -12,7 +12,7 @@ estudiantes = [
     {'nombre': 'Martha', 'sexo': 'F', 'carrera': 'soft', 'estado.': 'medio',
      'promedio': 100, 'asistencia': 100, 'beca': False, 'deuda': 0, 'tipo': ""},
     {'nombre': 'Camille', 'sexo': 'F', 'carrera': 'soft', 'estado.': 'bajo',
-     'promedio': 80, 'asistencia': 90, 'beca': False, 'deuda': 0, 'tipo': ""},
+     'promedio': 98, 'asistencia': 90, 'beca': False, 'deuda': 0, 'tipo': ""},
     {'nombre': 'Andres', 'sexo': 'M', 'carrera': 'med', 'estado.': 'alto',
      'promedio': 70, 'asistencia': 60, 'beca': False, 'deuda': 0, 'tipo': ""},
     {'nombre': 'Alison', 'sexo': 'f', 'carrera': 'bio', 'estado.': 'medio',
@@ -25,63 +25,58 @@ estudiantes = [
 def beca_academica():
     print("---------------------------------------------------------becas academicas------------------------------------------------------")
     for i in estudiantes:
-        if i['tipo'] == "":
-            sin_beca = True
-        else:
-            sin_beca = False
-        if 95 <= i['promedio'] <= 100:
-            cumplep = True
-        else:
-            cumplep = False
-        if 90 <= i['asistencia'] <= 100:
-            cumplea = True
-        else:
-            cumplea = False
-        if i['deuda'] == 0:
-            cumpled = True
-        else:
-            cumpled = False
-        if (cumplep & cumplea & cumpled & sin_beca):
-            i['beca'] = True
-            i['tipo'] = "a"
-            print(
-                f"beca otorgada a {i['nombre']}: {i['beca']}, tipo: {i['tipo']}  ")
-        
+        if i['beca'] == False:
+
+            if 95 <= i['promedio'] <= 100:
+                cumplep = True
+            else:
+                cumplep = False
+            if 90 <= i['asistencia'] <= 100:
+                cumplea = True
+            else:
+                cumplea = False
+            if i['deuda'] == 0:
+                cumpled = True
+            else:
+                cumpled = False
+            if (cumplep & cumplea & cumpled):
+                i['beca'] = True
+                i['tipo'] = "a"
+                print(
+                    f"beca otorgada a {i['nombre']}: {i['beca']}, tipo: {i['tipo']}  ")
 
 
 def beca_socioeconomica():
     print("-------------------------------------------------------becas socioeconomicas---------------------------------------------------")
     for i in estudiantes:
-        if i['tipo'] == "":
-            sin_beca = True
-        else:
-            sin_beca = False
-        if i['estado.'] == "bajo" or i['estado.'] == "medio bajo":
-            socioe = True
-        else:
-            socioe = False
-        if 80 <= i['promedio'] <= 100:
-            cumplep = True
-        else:
-            cumplep = False
-        if 90 <= i['asistencia'] <= 100:
-            cumplea = True
-        else:
-            cumplea = False
-        if i['deuda'] == 0:
-            cumpled = True
-        else:
-            cumpled = False
-        if (cumplep & cumplea & cumpled & sin_beca & socioe):
-            i['beca'] = True
-            i['tipo'] = "s.e."
-            print(
-                f"beca otorgada a {i['nombre']}: {i['beca']}, tipo: {i['tipo']}  ")
+        if i['beca'] == False:
+
+            if i['estado.'] == "bajo" or i['estado.'] == "medio bajo":
+                socioe = True
+            else:
+                socioe = False
+            if 80 <= i['promedio'] <= 100:
+                cumplep = True
+            else:
+                cumplep = False
+            if 90 <= i['asistencia'] <= 100:
+                cumplea = True
+            else:
+                cumplea = False
+            if i['deuda'] == 0:
+                cumpled = True
+            else:
+                cumpled = False
+            if (cumplep & cumplea & cumpled & socioe):
+                i['beca'] = True
+                i['tipo'] = "s.e."
+                print(
+                    f"beca otorgada a {i['nombre']}: {i['beca']}, tipo: {i['tipo']}  ")
 
 
 def run():
-    beca_socioeconomica()
     beca_academica()
+    beca_socioeconomica()
 
 
 if __name__ == "__main__":
