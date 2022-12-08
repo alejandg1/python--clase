398bc7e4b1b60abd6894fb0d8f9eb58af72c976a
Alejandro Gómez <josealejandrogomezparedes@gmail.com>
2022-12-07 19:53:44 -0500

    miercoles

---
+++ cosas aparte/camelcase.py
@@ -0,0 +1,7 @@
+def digital_root(n):
+    suma=0
+    for i in n:
+        suma +=(i)
+    print(suma)
+
+digital_root(24)
---
+++ cosas aparte/raiz_digital.py
@@ -0,0 +1,7 @@
+def RD(numero):
+    suma=0
+    for i in str(numero):
+        suma +=int(i)
+    print(suma)
+
+RD(15)
