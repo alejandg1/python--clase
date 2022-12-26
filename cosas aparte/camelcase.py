63fa2dc9762f4c70f019140fb5b79e708428a8da
alejandg1 <71184889+alejandg1@users.noreply.github.com>
2022-12-26 10:12:29 -0500

    Delete cosas aparte directory

--- cosas aparte/camelcase.py
+++
@@ -1,7 +0,0 @@
-def digital_root(n):
-    suma=0
-    for i in n:
-        suma +=(i)
-    print(suma)
-
-digital_root(24)
--- cosas aparte/raiz_digital.py
+++
@@ -1,7 +0,0 @@
-def RD(numero):
-    suma=0
-    for i in str(numero):
-        suma +=int(i)
-    print(suma)
-
-RD(15)
